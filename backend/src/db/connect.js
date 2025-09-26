4cffe3cf78676d98c7b0dbb347a59e4f18f16f72
Minh Nguyen <nhminh.ng@gmail.com>
2025-09-26 11:48:34 -0500

    feat: chart and sidebar

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: process.env.MONGO_DB_NAME || "myDatabase",
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
